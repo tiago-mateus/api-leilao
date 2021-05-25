673289c19a7831d15bd4d923ef5aea16f3a0f9ba
Tiago Mateus <tiagomateus.ti@gmail.com>
2021-05-25 20:40:10 -0300

    fix2

--- knexfile.js
+++ knexfile.js
@@ -31,20 +31,16 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      host:process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      directory: './src/database/migrations',
+      directory: __dirname+'\\database\\migrations',
       tableName: 'knex_migrations'
     }
   }
 
 };
+  
